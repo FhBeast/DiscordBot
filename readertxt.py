4bec40650cf61f31619904afa049029ab3c122a2
FhBeast <fhdavidqqq@gmail.com>
2022-11-20 00:56:20 +0300

    edit readertxt.py

--- readertxt.py
+++ readertxt.py
@@ -1,7 +1,7 @@
 def readtxt(txt):
-	lst = []
-	file = open(txt, "r", encoding="utf-8")
-	for line in file:
-		lst.append(line)
+    lst = []
+    file = open(txt, "r", encoding="utf-8")
+    for line in file:
+        lst.append(line)
     file.close()
     return lst
