322ea20eea1091faf0a7a28695fbdbda965dbb7d
FhBeast <fhdavidqqq@gmail.com>
2022-11-20 00:35:34 +0300

    edit

--- readertxt.py
+++ readertxt.py
@@ -1,11 +1,7 @@
-import os
-
 def readtxt(txt):
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, 'txt')
 	lst = []
 	try:
-		file = open(filename, "r")
+		file = open(txt, "r")
 		for line in file:
 			lst.append(line)
 		return lst
